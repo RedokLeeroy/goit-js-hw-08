938d55f581fe2397ac2edd6fb94718158d5b9e41
RedokLeeroy <Lys1999@ukr.net>
2022-06-20 20:50:52 +0300

    .

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const galleryBox = galleryItems.map(({preview , description , original}) => {
+    return `<a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>`
+});
+
+gallery.insertAdjacentHTML("afterbegin", galleryBox.join(""));
+
+new SimpleLightbox('.gallery a', {captionsData: "alt", captionDelay: 250 });
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+ const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+function callback(data) {
+    localStorage.setItem("videoplayer-current-time", data.seconds)
+}
+
+player.on('timeupdate', throttle(callback, 1000));
+
+const currtime = localStorage.getItem("videoplayer-current-time");
+player.setCurrentTime(currtime)
 
