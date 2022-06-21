487a2001a418aa080a80556dc26adc7641a869f8
RedokLeeroy <Lys1999@ukr.net>
2022-06-21 21:18:21 +0300

    O REKURSIA

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
+let data; 
+
+const STORAGE_KEY = "feedback-form-state"
+const form = document.querySelector(".feedback-form");
+const inputElone = document.querySelector("[name='email']");
+const inputEltwo = document.querySelector("[name='message']");
+
+
+form.addEventListener("input", handleform);
+function handleform (event) {
+    const {email, message} = event.currentTarget.elements
+    const obj = {
+        email: email.value,
+        message: message.value
+    }
+
+    data = localStorage.setItem(STORAGE_KEY, JSON.stringify(obj))
+}
+
+if(localStorage.getItem(STORAGE_KEY)){
+    const value =  JSON.parse(localStorage.getItem(STORAGE_KEY))
+    inputElone.value = value.email ;
+    inputEltwo.value = value.message;
+};
+
+form.addEventListener("submit", handlesubmit);
+function handlesubmit (event) {
+    event.preventDefault()
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+    
+    if(inputElone.value === ''||inputEltwo.value === '') {
+    return alert("Введи значення у всі поля")
+}
+    localStorage.removeItem(STORAGE_KEY)
+    event.target.reset();
+
+}
 
